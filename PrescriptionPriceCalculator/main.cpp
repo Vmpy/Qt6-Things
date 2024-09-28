75bf7d117df22e57064bca18c711f5a11cf23cdc
Ian <mupengyi@foxmail.com>
2024-09-28 22:39:45 +0800

    Update main.cpp

--- PrescriptionPriceCalculator/main.cpp
+++ PrescriptionPriceCalculator/main.cpp
@@ -8,6 +8,8 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
 
+    QIcon icon("./icon.png");
+    w.setWindowIcon(icon);
     w.showMaximized();
     return a.exec();
 }
